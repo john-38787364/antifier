9c331a7f617fa8b0582d5c50ad6326b8887400eb
John <john+tacx@vetsurgeon.org.uk>
2017-10-10 19:27:23 +0100

    posix install script

--- install.sh
+++ install.sh
@@ -1,6 +1,13 @@
+apt-get install python-pip
+pip install pyserial
+mkdir tacx-interface
+cd tacx-interface
+wget --quiet https://raw.githubusercontent.com/john-38787364/tacx-ant/master/tacx-interface.py
+wget --quiet https://raw.githubusercontent.com/john-38787364/tacx-ant/master/ant.py
+wget --quiet https://raw.githubusercontent.com/john-38787364/tacx-ant/master/T1932_calibration.py
 echo "##########################################################################"
-echo "This script will install python-pip and pyserial. It will then create the"
-echo "directory tacx-interface and download the required files."
+echo "This script has installed python-pip and pyserial. It has created the"
+echo "directory tacx-interface and downloaded the required files."
 echo "Run via sudo python ./tacx-interface.py"
 echo "Options:"
 echo "--debug to provide more output to screen"
@@ -9,14 +16,5 @@ echo "cadence=90rpm, power=283W, HT 72BPM"
 echo "--power-factor to increase or decrease power broadcast."
 echo "--power-factor=1.5 will result in a broadcast of 150W for 100W read at wheel"
 echo "##########################################################################"
-read -p "Press any key to start"
-apt-get install python-pip
-pip install pyserial
-mkdir tacx-interface
-cd tacx-interface
-wget https://raw.githubusercontent.com/john-38787364/tacx-ant/master/tacx-interface.py
-wget https://raw.githubusercontent.com/john-38787364/tacx-ant/master/ant.py
-wget https://raw.githubusercontent.com/john-38787364/tacx-ant/master/T1932_calibration.py
 echo "Installation finished"
 echo "Now cd into tacx-interface and run sudo python ./tacx-interface.py"
-read -p "Press any key to finish"
