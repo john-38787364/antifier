bee03deb3cd1a901e97983361a754406448d2043
JohnT <john+tacx@vetsurgeon.org.uk>
2017-10-19 16:59:12 +0100

    power curve gui

--- ant.py
+++ ant.py
@@ -1,4 +1,4 @@
-import binascii, re, os
+import binascii, re, os, usb.core, glob, serial
 
 def calc_checksum(message):#calulate message checksum
   pattern = re.compile('[\W_]+')
@@ -80,3 +80,102 @@ def second_channel_config(dev_ant):
   ]
   send(stringl, dev_ant, False)
 
+def powerdisplay(dev_ant):
+  #calibrate as power display
+  stringl=[
+  "a4 03 42 00 00 00 e5 00 00", #42 assign channel
+  "a4 05 51 00 00 00 0b 00 fb 00 00", #51 set channel id, 0b device=power sensor
+  "a4 02 45 00 39 da 00 00", #45 channel freq
+  "a4 03 43 00 f6 1f 0d 00 00", #43 msg period
+  "a4 02 71 00 00 d7 00 00", #71 Set Proximity Search chann number 0 search threshold 0
+  "a4 02 63 00 0a cf 00 00", #63 low priority search channel number 0 timeout 0
+  "a4 02 44 00 02 e0 00 00", #44 Host Command/Response 
+  "a4 01 4b 00 ee 00 00" #4b ANT_OpenChannel message ID channel = 0 D00001229_Fitness_Modules_ANT+_Application_Note_Rev_3.0.pdf
+  ]
+  send(stringl, dev_ant, False)
+
+def get_ant():
+  ###windows###
+  if os.name == 'nt':
+    found_available_ant_stick= True
+    try:
+      dev_ant = usb.core.find(idVendor=0x0fcf, idProduct=0x1009) #get ANT+ stick (garmin)
+      dev_ant.set_configuration() #set active configuration
+      try:#check if in use
+        stringl=["a4 01 4a 00 ef 00 00"]#reset system
+        ant.send(stringl, dev_ant, debug)
+        msg = "Using Garmin dongle..."
+      except usb.core.USBError:
+        found_available_ant_stick = False
+    except AttributeError:
+      #print "No Garmin Device found"
+      found_available_ant_stick = False
+
+    if found_available_ant_stick == False:
+      found_available_ant_stick = True
+      try:
+        dev_ant = usb.core.find(idVendor=0x0fcf, idProduct=0x1008) #get ANT+ stick (suunto)
+        dev_ant.set_configuration() #set active configuration   
+        try:#check if in use
+          stringl=["a4 01 4a 00 ef 00 00"]#reset system
+          ant.send(stringl, dev_ant, False)
+          msg = "Using Suunto dongle..."
+        except usb.core.USBError:
+          #print "Suunto Device is in use"
+          found_available_ant_stick = False
+      except AttributeError:  
+        #print "No Suunto Device found"
+        found_available_ant_stick = False
+
+    if found_available_ant_stick == False:
+      dev_ant = False 
+
+  ###Linux###
+  elif os.name == 'posix':
+    #Find ANT+ USB stick on serial (Linux)
+    ant_stick_found = False
+    for p in glob.glob('/dev/ttyUSB*'):
+      dev_ant = serial.Serial(p, 19200, rtscts=True,dsrdtr=True)
+      dev_ant.timeout = 0.1
+      dev_ant.write(binascii.unhexlify("a4014a00ef0000")) #probe with reset command
+      reply = binascii.hexlify(dev_ant.read(size=256))
+      if reply == "a4016f20ea" or reply == "a4016f00ca":#found ANT+ stick
+        serial_port=p
+        ant_stick_found = True
+        msg = "Found ANT Stick"
+      else: dev_ant.close()#not correct reply to reset
+      if ant_stick_found == True  : break
+
+    if ant_stick_found == False:
+      #print 'Could not find ANT+ device. Check output of "lsusb | grep 0fcf" and "ls /dev/ttyUSB*"'
+      #sys.exit()
+      dev_ant = False
+      
+    
+  else:
+    #print "OS not Supported"
+    #sys.exit()
+    dev_ant = False
+  
+  
+  if not dev_ant: msg = "ANT Stick not found"
+  return dev_ant, msg
+
+def get_trainer():
+  product=0
+  idpl = [0x1932, 0x1942]#iflow, fortius
+  for idp in idpl:
+    dev = usb.core.find(idVendor=0x3561, idProduct=idp) #find iflow device
+    if dev != None:
+      product=idp
+      break
+    
+  if product == 0:
+    return False
+  else:
+    return dev
+  
+def initialise_trainer(dev):
+  byte_ints = [2,0,0,0] # will not read cadence until initialisation byte is sent
+  byte_str = "".join(chr(n) for n in byte_ints)
+  dev.write(0x02,byte_str)
