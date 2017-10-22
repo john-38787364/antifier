ad9c91ac8c321784f8be5b2ee4d53525b6bc82d3
john-38787364 <32509693+john-38787364@users.noreply.github.com>
2017-10-22 22:59:39 +0100

    added timout to read/writes

--- trainer.py
+++ trainer.py
@@ -22,12 +22,12 @@ def send(dev_trainer, grade, pedecho=0, resistance_level_override=False):
     r5=int(reslist[resistance_level]) & 0xff #byte 5
     byte_ints = [0x01, 0x08, 0x01, 0x00, r5, r6, pedecho, 0x00 ,0x02, 0x52, 0x10, 0x04]
     byte_str = "".join(chr(n) for n in byte_ints)
-    dev_trainer.write(0x02,byte_str)#send data to device
+    dev_trainer.write(0x02, byte_str, 30)#send data to device
     return resistance_level
   
 def receive(dev_trainer):
   global trainer_type, possfov, factors
-  data = dev_trainer.read(0x82,64)
+  data = dev_trainer.read(0x82, 64, 30)
   if trainer_type == 0x1932:
     if len(data)>40:
       fs = int(data[33])<<8 | int(data[32])
