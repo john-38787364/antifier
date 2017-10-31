3b479abfaa7491f31fb19aaab3c1348e008f9fe8
John <john+tacx@vetsurgeon.org.uk>
2017-10-31 22:29:31 +0000

    clean up

--- keypress.py
+++
@@ -1,9 +0,0 @@
-import win32com.client, time
-time.sleep(5)
-shell = win32com.client.Dispatch("WScript.Shell")
-shell.AppActivate("Zwift")
-for a in range(1,9):
-  shell.SendKeys(a) # CTRL+A may "select all" depending on which window's focused
-  time.sleep(2)
-#shell.SendKeys("{DELETE}") # Delete selected text?  Depends on context. :P
-#shell.SendKeys("{TAB}") #Press tab... to change focus or whatever
