d541cfe6b29e04d3a58c399344596f522e1364fb
John <john@vetsurgeon.org.uk>
2017-11-03 23:18:07 +0000

    32 bit build

---
+++ build_windows_package_x32.bat
@@ -0,0 +1,4 @@
+c:\Python27\Scripts\pyinstaller.exe --onefile antifier.py
+c:\Python27\Scripts\pyinstaller.exe --onefile tacx_trainer_debug.py
+c:\Python27\Scripts\pyinstaller.exe --onefile power_curve.py
+iexpress /N antifier_package_x32.SED
