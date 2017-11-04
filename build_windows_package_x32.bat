38fedcf2e3d44ca852d2117d5b6701784e5ae4f8
John <john@vetsurgeon.org.uk>
2017-11-04 11:07:58 +0000

    sort build directories

--- build_windows_package_x32.bat
+++ build_windows_package_x32.bat
@@ -1,4 +1,5 @@
-c:\Python27\Scripts\pyinstaller.exe --onefile antifier.py
-c:\Python27\Scripts\pyinstaller.exe --onefile tacx_trainer_debug.py
-c:\Python27\Scripts\pyinstaller.exe --onefile power_curve.py
+c:\Python27\Scripts\pyinstaller.exe --distpath=.\build_x32\ --onefile antifier.py
+c:\Python27\Scripts\pyinstaller.exe --distpath=.\build_x32\ --onefile tacx_trainer_debug.py
+c:\Python27\Scripts\pyinstaller.exe --distpath=.\build_x32\ --onefile power_curve.py
+rd /s /q .\build
 iexpress /N antifier_package_x32.SED
--- build_windows_package_x64.bat
+++ build_windows_package_x64.bat
@@ -1,4 +1,5 @@
-c:\Python27\Scripts\pyinstaller.exe --onefile antifier.py
-c:\Python27\Scripts\pyinstaller.exe --onefile tacx_trainer_debug.py
-c:\Python27\Scripts\pyinstaller.exe --onefile power_curve.py
+c:\Python27\Scripts\pyinstaller.exe --distpath=.\build_x64\ --onefile antifier.py
+c:\Python27\Scripts\pyinstaller.exe --distpath=.\build_x64\ --onefile tacx_trainer_debug.py
+c:\Python27\Scripts\pyinstaller.exe --distpath=.\build_x64\ --onefile power_curve.py
+rd /s /q .\build
 iexpress /N antifier_package_x64.SED
